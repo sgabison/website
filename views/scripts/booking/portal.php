ccc91649efae012a3ef2dded6d3c372c7e57430a
Didier <didier.rechatin@gmail.com>
2015-10-08 14:28:51 +0200

    Thursday
    Change portal display

--- views/scripts/booking/portal.php
+++ views/scripts/booking/portal.php
@@ -45,6 +45,50 @@
 					
 				</div>
 			</div>
+			<div class="col-lg-4 col-md-5">
+				<div class="panel panel-white">
+					<div class="panel-heading border-light">
+						<h4 class="panel-title">La Criée</h4>
+<!--
+						<div class="panel-tools">
+							<div class="dropdown">
+								<a data-toggle="dropdown"
+									class="btn btn-xs dropdown-toggle btn-transparent-white"> <i
+									class="fa fa-cog"></i>
+								</a>
+								<ul class="dropdown-menu dropdown-light pull-right" role="menu">
+									<li><a class="panel-collapse collapses" href="#"><i
+											class="fa fa-angle-up"></i> <span>Collapse</span> </a>
+									</li>
+									<li><a class="panel-refresh" href="#"> <i class="fa fa-refresh"></i>
+											<span>Refresh</span>
+									</a>
+									</li>
+									<li><a class="panel-config" href="#panel-config"
+										data-toggle="modal"> <i class="fa fa-wrench"></i> <span>Configurations</span>
+									</a>
+									</li>
+									<li><a class="panel-expand" href="#"> <i class="fa fa-expand"></i>
+											<span>Fullscreen</span>
+									</a>
+									</li>
+								</ul>
+							</div>
+							<a class="btn btn-xs btn-link panel-close" href="#"> <i
+								class="fa fa-times"></i>
+							</a>
+						</div>
+-->
+					</div>
+					<div class="panel-body">
+						<iframe
+							src="http://www.lacriee.com/"
+							width="100%" height="600px" frameborder="0" id="iframe">
+							<p>Votre navigateur ne supporte pas l'élément iframe</p>
+						</iframe>
+					</div>
+				</div>
+			</div>
 			<div class="col-lg-4 col-md-5 col-sm-6">
 				<div class="row">
 					<div class="col-md-6">
@@ -146,50 +190,6 @@
 								</div>
 							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 col-md-5">
-				<div class="panel panel-white">
-					<div class="panel-heading border-light">
-						<h4 class="panel-title">La Criée</h4>
-<!--
-						<div class="panel-tools">
-							<div class="dropdown">
-								<a data-toggle="dropdown"
-									class="btn btn-xs dropdown-toggle btn-transparent-white"> <i
-									class="fa fa-cog"></i>
-								</a>
-								<ul class="dropdown-menu dropdown-light pull-right" role="menu">
-									<li><a class="panel-collapse collapses" href="#"><i
-											class="fa fa-angle-up"></i> <span>Collapse</span> </a>
-									</li>
-									<li><a class="panel-refresh" href="#"> <i class="fa fa-refresh"></i>
-											<span>Refresh</span>
-									</a>
-									</li>
-									<li><a class="panel-config" href="#panel-config"
-										data-toggle="modal"> <i class="fa fa-wrench"></i> <span>Configurations</span>
-									</a>
-									</li>
-									<li><a class="panel-expand" href="#"> <i class="fa fa-expand"></i>
-											<span>Fullscreen</span>
-									</a>
-									</li>
-								</ul>
-							</div>
-							<a class="btn btn-xs btn-link panel-close" href="#"> <i
-								class="fa fa-times"></i>
-							</a>
-						</div>
--->
-					</div>
-					<div class="panel-body">
-						<iframe
-							src="http://www.lacriee.com/"
-							width="100%" height="600px" frameborder="0" id="iframe">
-							<p>Votre navigateur ne supporte pas l'élément iframe</p>
-						</iframe>
 					</div>
 				</div>
 			</div>
